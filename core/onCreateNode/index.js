a051a495a9186597370c960d549ee42c3319841b
WeZZard <me@wezzard.com>
2019-01-24 23:58:47 +0800

    Removes the handler for config.yaml node creation.

--- core/onCreateNode/index.js
+++ core/onCreateNode/index.js
@@ -1,9 +1,7 @@
 const onCreateMDXDocuments = require('./onCreateMDXDocuments');
-const onCreateConfigYaml = require('./onCreateConfigYaml');
 
 module.exports = function (arg) {
     [
         onCreateMDXDocuments,
-        onCreateConfigYaml,
     ].forEach(_ => _(arg));
 };
