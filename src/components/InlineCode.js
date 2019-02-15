fb986850986c71a43635df2084dd843b1dd7b9d8
WeZZard <me@wezzard.com>
2019-02-16 06:14:36 +0800

    Fixes inline code styles.

--- src/components/InlineCode.js
+++ src/components/InlineCode.js
@@ -1,4 +1,8 @@
 import React from 'react';
 import styles from './InlineCode.module.scss'
 
-export default props => <code className={styles.inlineCode} {...props}/>
+export default ({children}) => {
+    return <code className={styles.inlineCodeWrapper}>
+        <span className={styles.inlineCode}>{children}</span>
+    </code>
+}
