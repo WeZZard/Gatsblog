5679d7a4ff9d5fe31d37de1659c91757f7c643dc
WeZZard <me@wezzard.com>
2019-02-17 00:53:36 +0800

    Removes debug logging.

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,7 +5,6 @@ import InlineSegment from './InlineSegment'
 
 export default props => {
     const { type, children } = props;
-    console.log(children);
     return <li className={[styles.listItem, styles[type]].join(' ')}>
         <InlineSegment children={children}/>
     </li>
