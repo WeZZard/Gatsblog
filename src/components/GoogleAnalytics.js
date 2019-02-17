98d5d9b7e9f282b4529e5d5ab630b12b8fa571c4
WeZZard <me@wezzard.com>
2019-02-18 00:10:32 +0800

    Changes Google Analytics setup appraoch.

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -1,48 +1,21 @@
 import React from 'react'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
 
-const GoogleAnalytics = () => {
-    return (
-        <StaticQuery
-            query={componentQuery}
-            render={data => {
-
-                const {
-                    config: {
-                        GoogleAnalytics
-                    },
-                } = data;
-
-                if (GoogleAnalytics.trackingID) {
-                    return (
-                        <Helmet>
-                            <script>
-                                {`
-                                window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-                                ga('create', '${GoogleAnalytics.trackingID}', 'auto');
-                                ga('send', 'pageview');
-                                `}
-                            </script>
-                            <script async src='https://www.google-analytics.com/analytics.js'/>
-                        </Helmet>
-                    )
-                } else {
-                    return null;
-                }
-            }}
-        />
-    )
-};
-
-export default GoogleAnalytics
-
-const componentQuery = graphql`
-    query GoogleAnalyticsQuery {
-        config: configYaml {
-            GoogleAnalytics {
-                trackingID
-            }
-        }
+export default () => {
+    if (process.env.GATSBY_GA_TRACKING_ID) {
+        return (
+            <Helmet>
+                <script>
+                    {`
+                    window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+                    ga('create', '${process.env.GATSBY_GA_TRACKING_ID}', 'auto');
+                    ga('send', 'pageview');
+                    `}
+                </script>
+                <script async src='https://www.google-analytics.com/analytics.js'/>
+            </Helmet>
+        )
+    } else {
+        return null;
     }
-`;
+}
