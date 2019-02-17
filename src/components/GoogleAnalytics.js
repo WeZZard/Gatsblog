efb159b86a66c466cc791fe2db4748f9836d2652
WeZZard <me@wezzard.com>
2019-02-18 00:31:01 +0800

    Refines Google Analytics.

--- src/components/GoogleAnalytics.js
+++ src/components/GoogleAnalytics.js
@@ -6,11 +6,11 @@ export default () => {
         return (
             <Helmet>
                 <script>
-                    {`
-                    window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
-                    ga('create', '${process.env.GATSBY_GA_TRACKING_ID}', 'auto');
-                    ga('send', 'pageview');
-                    `}
+                    {
+`window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+ga('create', '${process.env.GATSBY_GA_TRACKING_ID}', 'auto');
+ga('send', 'pageview');`
+}
                 </script>
                 <script async src='https://www.google-analytics.com/analytics.js'/>
             </Helmet>
